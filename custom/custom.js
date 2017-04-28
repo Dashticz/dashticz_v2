468b4332f0fb47770ac0d23d1738dbf32bdd17bb
Rob Geerts <robgeerts@gmail.com>
2017-04-28 22:25:57 +0200

    caching

--- custom/custom.js
+++ custom/custom.js
@@ -5,5 +5,6 @@ function afterGetDevices(){
 }
 
 function getExtendedBlockTypes(blocktypes){
+	//blocktypes.Type['Lighting 2'] = { icon: 'fa fa-lightbulb-o', title: '<Name>', value: 'ds' }
 	return blocktypes;
 }
