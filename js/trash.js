5d6a11a8093a7bcacf06ba4b10d29643ba10a575
Rob Geerts <robgeerts@gmail.com>
2017-05-04 16:47:15 +0200

    Trash Cure

--- js/trash.js
+++ js/trash.js
@@ -21,8 +21,8 @@ function loadTrash (random,trashobject) {
 		html+='</div>';
 	html+='</div>';
 	
+	var returnDates={};
 	if(service=='deafvalapp'){
-		var returnDates={};
 		$.get('https://cors-anywhere.herokuapp.com/http://dataservice.deafvalapp.nl/dataservice/DataServiceServlet?type=ANDROID&service=OPHAALSCHEMA&land=' +
 			country + '&postcode=' + postcode + '&straatId=0&huisnr=' + homenumber + '&huisnrtoev=',function(data){
 			var respArray = data.toString().split('\n').join('').split(";");
@@ -42,7 +42,7 @@ function loadTrash (random,trashobject) {
 						if(typeof(returnDates[curr])=='undefined'){
 							returnDates[curr] = {}
 						}
-						returnDates[curr][moment(moment(respArray[i], "DD-MM-YYYY")).format("YYYY-MM-DD")]='<div>'+curr+': '+respArray[i]+'</div>'
+						returnDates[curr][moment(moment(respArray[i], "DD-MM-YYYY")).format("YYYY-MM-DD")]='<div>'+curr+': '+respArray[i]+'</div>';
 					}
 				}
 			}
@@ -65,7 +65,41 @@ function loadTrash (random,trashobject) {
 			});
 		});
 	}
-	
+	if(service=='cure'){
+		$('.trash'+random+' .state').html('');
+		var returnDates={};
+		$.getJSON('https://afvalkalender.cure-afvalbeheer.nl/rest/adressen/' + postcode + '-' + homenumber,function(data){
+			$.getJSON('https://afvalkalender.cure-afvalbeheer.nl/rest/adressen/'+data[0].bagId+'/afvalstromen',function(data){
+				for(d in data){
+					if(data[d]['ophaaldatum']!==null){
+						if(typeof(returnDates[curr])=='undefined'){
+							returnDates[curr] = {}
+						}
+						returnDates[curr][moment(moment(data[d]['ophaaldatum'], "YYYY-MM-DD")).format("YYYY-MM-DD")]='<div>'+data[d]['menu_title']+': '+moment(moment(data[d]['ophaaldatum'], "YYYY-MM-DD")).format("DD-MM-YYYY")+'</div>';
+					
+						//$('.trash'+random+' .state').append('<div>'+data[d]['menu_title']+': '+moment(moment(data[d]['ophaaldatum'], "YYYY-MM-DD")).format("DD-MM-YYYY")+'</div>');
+					}
+				}
+				
+				var returnDatesSimple={}
+				for(c in returnDates){
+					for(cr in returnDates[c]){
+						//if(typeof(done[c])=='undefined'){
+							returnDatesSimple[cr] = returnDates[c][cr];
+							done[c]=true;
+						//}
+					}
+				}
+
+				$('.trash'+random+' .state').html('');
+				var c=1;
+				Object.keys(returnDatesSimple).sort().forEach(function(key) {
+					if(c<=3) $('.trash'+random+' .state').append(returnDatesSimple[key]);
+					c++;
+				});
+			});
+		});
+	}
 	return html;
 			
 }
