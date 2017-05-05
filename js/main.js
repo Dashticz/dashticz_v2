f3a49956d5d9ec6bb8d03074c1e96fa4a462adf1
Rob Geerts <robgeerts@gmail.com>
2017-05-05 13:13:35 +0200

    stationclock width

--- js/main.js
+++ js/main.js
@@ -286,7 +286,7 @@ function getBlock(cols,c,columndiv,standby){
 				$(columndiv).append('<div data-id="clock" class="transbg block_'+cols['blocks'][b]+' col-xs-'+width+' text-center"><h1 class="clock"></h1><h4 class="weekday"></h4><h4 class="date"></h4></div>');
 			}
 			else if(cols['blocks'][b]=='stationclock'){
-				$(columndiv).append('<div data-id="clock" class="transbg block_'+cols['blocks'][b]+' col-xs-'+width+' text-center"><canvas id="clock" width="200" height="200">Your browser is unfortunately not supported.</canvas></div>');
+				$(columndiv).append('<div data-id="clock" class="transbg block_'+cols['blocks'][b]+' col-xs-'+width+' text-center"><canvas id="clock" width="150" height="150">Your browser is unfortunately not supported.</canvas></div>');
 				if(typeof(StationClock)!=='function') $.ajax({url: 'vendor/stationclock.js', async: false,dataType: "script"});
 				
 				var clock = new StationClock("clock");
