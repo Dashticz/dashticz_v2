e64190c3980a0aa20028e46f58c7bd5260223031
Rob Geerts <robgeerts@gmail.com>
2017-05-05 09:01:38 +0200

    Added Italian language file

---
+++ lang/it_IT.js
@@ -0,0 +1,53 @@
+
+var lang = new Object();
+lang['state_on'] = 'ON';
+lang['state_off'] = 'OFF';
+lang['state_open'] = 'APERTO';
+lang['state_closed'] = 'CHIUSO';
+lang['state_movement'] = 'MOVIMENTO';
+lang['state_nomovement'] = 'No movimento';
+lang['state_switch'] = 'Switch';
+lang['state_nosmoke'] = 'No fumo';
+lang['state_smoke'] = 'FUMO!';
+
+lang['monday'] = 'Lunedì';
+lang['tuesday'] = 'Martedì';
+lang['wednesday'] = 'Mercoledì';
+lang['thursday'] = 'Giovedì';
+lang['friday'] = 'Venerdì';
+lang['saturday'] = 'Sabato';
+lang['sunday'] = 'Domenica';
+
+lang['graph_last_hours'] = 'ultime ore';
+lang['graph_today'] = 'oggi';
+lang['graph_last_month'] = 'ultimo mese';
+
+lang['mediaplayer_nothing_playing'] = 'Non in riproduzione ora';
+lang['energy_usage'] = 'Corrente';
+lang['energy_usagetoday'] = 'Corrente oggi';
+lang['energy_totals'] = 'Totale';
+lang['energy_total'] = 'totale';
+lang['energy_now'] = 'ora';
+lang['energy_today'] = 'oggi';
+lang['gas_usagetoday'] = 'Gas oggi';
+lang['temp_toon'] = 'Salone';
+lang['wind'] = 'Vento';
+lang['select'] = 'Seleziona';
+lang['notifications_ns'] = 'notifica aggiunta da NS';
+
+lang['direction_N'] = 'nord'
+lang['direction_NNE'] = 'nord-nordest';
+lang['direction_NE'] = 'nordest'
+lang['direction_ENE'] = 'est-nordest';
+lang['direction_E'] = 'est';
+lang['direction_ESE'] = 'est-sudest';
+lang['direction_SE'] = 'sudest';
+lang['direction_SSE'] = 'sud-sudest';
+lang['direction_S'] = 'sud' ;
+lang['direction_SSW'] = 'sud-sudovest';
+lang['direction_SW'] = 'sud-ovest';
+lang['direction_WSW'] = 'ovest-sudovest';
+lang['direction_W'] = 'ovest';
+lang['direction_WNW'] = 'ovest-nordovest';
+lang['direction_NW'] = 'nordovest';
+lang['direction_NNW'] = 'nord-nordovest';
