0653e03916d63e85e60907d4b7df955705fb7d21
Rob Geerts <robgeerts@gmail.com>
2017-05-11 09:40:42 +0200

    custom.js

---
+++ custom/custom.js
@@ -0,0 +1,10 @@
+ //add custom javascript in here
+function afterGetDevices(){
+	
+	
+}
+
+function getExtendedBlockTypes(blocktypes){
+	//blocktypes.Type['Lighting 2'] = { icon: 'fa fa-lightbulb-o', title: '<Name>', value: 'ds' }
+	return blocktypes;
+} 
