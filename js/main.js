852f10b059e5b8bb10da3a94249609162295cdfb
Rob Geerts <robgeerts@gmail.com>
2017-05-29 09:23:39 +0200

    error log in console

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ window.onerror = function(msg, url, line, col, error) {
    var extra = !col ? '' : '\ncolumn: ' + col;
    extra += !error ? '' : '\nerror: ' + error;
 
-   alert("Error: " + msg + "\nurl: " + url + "\nline: " + line);
+   console.error("Error: " + msg + "\nurl: " + url + "\nline: " + line);
 
    var suppressErrorAlert = true;
    return suppressErrorAlert;
