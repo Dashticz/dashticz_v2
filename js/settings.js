54365142eba5d1f77380671e2df939415b721a7c
Rob Geerts <robgeerts@gmail.com>
2017-07-20 22:57:41 +0200

    settings tabs

--- js/settings.js
+++ js/settings.js
@@ -10,36 +10,14 @@ settingList['general']['app_title'] = {}
 settingList['general']['app_title']['title'] = 'App title';
 settingList['general']['app_title']['type'] = 'text';
 
-settingList['general']['language'] = {}
-settingList['general']['language']['title'] = 'Language';
-settingList['general']['language']['type'] = 'select';
-settingList['general']['language']['options'] = {};
-settingList['general']['language']['options']['zh_CN'] = 'Chinese';
-settingList['general']['language']['options']['nl_NL'] = 'Dutch';
-settingList['general']['language']['options']['en_US'] = 'English';
-settingList['general']['language']['options']['fr_FR'] = 'French';
-settingList['general']['language']['options']['hu_HU'] = 'Hungarian';
-settingList['general']['language']['options']['it_IT'] = 'Italian';
-settingList['general']['language']['options']['pt_PT'] = 'Portugese';
-settingList['general']['language']['options']['sv_SE'] = 'Swedish';
-
-settingList['general']['hide_topbar'] = {}
-settingList['general']['hide_topbar']['title'] = 'Hide topbar';
-settingList['general']['hide_topbar']['type'] = 'checkbox';
-
-settingList['general']['auto_positioning'] = {}
-settingList['general']['auto_positioning']['title'] = 'Auto Positioning?';
-settingList['general']['auto_positioning']['type'] = 'checkbox';
-settingList['general']['auto_positioning']['help'] = 'Don\'t want to configure positions, use auto positioning';
+settingList['general']['selector_instead_of_buttons'] = {}
+settingList['general']['selector_instead_of_buttons']['title'] = 'Selector instead of buttons';
+settingList['general']['selector_instead_of_buttons']['type'] = 'checkbox';
 
 settingList['general']['use_favorites'] = {}
 settingList['general']['use_favorites']['title'] = 'Only use favorite devices';
 settingList['general']['use_favorites']['type'] = 'checkbox';
 settingList['general']['use_favorites']['help'] = 'Only used when using auto positioning';
-
-settingList['general']['standby_after'] = {}
-settingList['general']['standby_after']['title'] = 'Standby after (minutes, 0 = never)';
-settingList['general']['standby_after']['type'] = 'text';
 
 settingList['general']['domoticz_refresh'] = {}
 settingList['general']['domoticz_refresh']['title'] = 'Refresh Domoticz (seconds)';
@@ -49,115 +27,133 @@ settingList['general']['dashticz_refresh'] = {}
 settingList['general']['dashticz_refresh']['title'] = 'Refresh Dashticz (minutes)';
 settingList['general']['dashticz_refresh']['type'] = 'text';
 
-settingList['general']['auto_swipe_back_to'] = {}
-settingList['general']['auto_swipe_back_to']['title'] = 'Auto swipe back to screen #';
-settingList['general']['auto_swipe_back_to']['type'] = 'text';
-settingList['general']['auto_swipe_back_to']['help'] = 'When no activity, swipe back to main screen after x seconds';
+settingList['screen'] = {}
+settingList['screen']['title'] = 'Screen';
 
-settingList['general']['auto_swipe_back_after'] = {}
-settingList['general']['auto_swipe_back_after']['title'] = 'Auto swipe back after (seconds)';
-settingList['general']['auto_swipe_back_after']['type'] = 'text';
+settingList['screen']['hide_topbar'] = {}
+settingList['screen']['hide_topbar']['title'] = 'Hide topbar';
+settingList['screen']['hide_topbar']['type'] = 'checkbox';
 
-settingList['general']['auto_slide_pages'] = {}
-settingList['general']['auto_slide_pages']['title'] = 'Auto slide screens (seconds, 0=do not auto slide)';
-settingList['general']['auto_slide_pages']['type'] = 'text';
+settingList['screen']['auto_positioning'] = {}
+settingList['screen']['auto_positioning']['title'] = 'Auto Positioning?';
+settingList['screen']['auto_positioning']['type'] = 'checkbox';
+settingList['screen']['auto_positioning']['help'] = 'Don\'t want to configure positions, use auto positioning';
 
-settingList['general']['slide_effect'] = {}
-settingList['general']['slide_effect']['title'] = 'Slide effect';
-settingList['general']['slide_effect']['type'] = 'select';
-settingList['general']['slide_effect']['options'] = {};
-settingList['general']['slide_effect']['options']['slide'] = 'slide';
-settingList['general']['slide_effect']['options']['fade'] = 'fade';
-settingList['general']['slide_effect']['options']['cube'] = 'cube';
-settingList['general']['slide_effect']['options']['coverflow'] = 'coverflow';
-settingList['general']['slide_effect']['options']['flip'] = 'flip';
+settingList['screen']['standby_after'] = {}
+settingList['screen']['standby_after']['title'] = 'Standby after (minutes, 0 = never)';
+settingList['screen']['standby_after']['type'] = 'text';
 
-settingList['general']['selector_instead_of_buttons'] = {}
-settingList['general']['selector_instead_of_buttons']['title'] = 'Selector instead of buttons';
-settingList['general']['selector_instead_of_buttons']['type'] = 'checkbox';
+settingList['screen']['auto_swipe_back_to'] = {}
+settingList['screen']['auto_swipe_back_to']['title'] = 'Auto swipe back to screen #';
+settingList['screen']['auto_swipe_back_to']['type'] = 'text';
+settingList['screen']['auto_swipe_back_to']['help'] = 'When no activity, swipe back to main screen after x seconds';
 
-settingList['general']['hide_mediaplayer'] = {}
-settingList['general']['hide_mediaplayer']['title'] = 'Hide mediaplayer when it\'s off';
-settingList['general']['hide_mediaplayer']['type'] = 'checkbox';
+settingList['screen']['auto_swipe_back_after'] = {}
+settingList['screen']['auto_swipe_back_after']['title'] = 'Auto swipe back after (seconds)';
+settingList['screen']['auto_swipe_back_after']['type'] = 'text';
 
-settingList['general']['use_fahrenheit'] = {}
-settingList['general']['use_fahrenheit']['title'] = 'Use Fahrenheit instead of Celcius';
-settingList['general']['use_fahrenheit']['type'] = 'checkbox';
+settingList['screen']['auto_slide_pages'] = {}
+settingList['screen']['auto_slide_pages']['title'] = 'Auto slide screens (seconds, 0=do not auto slide)';
+settingList['screen']['auto_slide_pages']['type'] = 'text';
 
-settingList['general']['use_beaufort'] = {}
-settingList['general']['use_beaufort']['title'] = 'Use Bft instead of m/s';
-settingList['general']['use_beaufort']['type'] = 'checkbox';
+settingList['screen']['slide_effect'] = {}
+settingList['screen']['slide_effect']['title'] = 'Slide effect';
+settingList['screen']['slide_effect']['type'] = 'select';
+settingList['screen']['slide_effect']['options'] = {};
+settingList['screen']['slide_effect']['options']['slide'] = 'slide';
+settingList['screen']['slide_effect']['options']['fade'] = 'fade';
+settingList['screen']['slide_effect']['options']['cube'] = 'cube';
+settingList['screen']['slide_effect']['options']['coverflow'] = 'coverflow';
+settingList['screen']['slide_effect']['options']['flip'] = 'flip';
 
-settingList['clocks'] = {}
-settingList['clocks']['title'] = 'Clocks';
+settingList['localize'] = {}
+settingList['localize']['title'] = 'Localize';
 
-settingList['clocks']['hide_seconds'] = {}
-settingList['clocks']['hide_seconds']['title'] = 'Hide seconds';
-settingList['clocks']['hide_seconds']['type'] = 'checkbox';
+settingList['localize']['language'] = {}
+settingList['localize']['language']['title'] = 'Language';
+settingList['localize']['language']['type'] = 'select';
+settingList['localize']['language']['options'] = {};
+settingList['localize']['language']['options']['zh_CN'] = 'Chinese';
+settingList['localize']['language']['options']['nl_NL'] = 'Dutch';
+settingList['localize']['language']['options']['en_US'] = 'English';
+settingList['localize']['language']['options']['fr_FR'] = 'French';
+settingList['localize']['language']['options']['hu_HU'] = 'Hungarian';
+settingList['localize']['language']['options']['it_IT'] = 'Italian';
+settingList['localize']['language']['options']['pt_PT'] = 'Portugese';
+settingList['localize']['language']['options']['sv_SE'] = 'Swedish';
 
-settingList['clocks']['hide_seconds_stationclock'] = {}
-settingList['clocks']['hide_seconds_stationclock']['title'] = 'Hide seconds in StationClock';
-settingList['clocks']['hide_seconds_stationclock']['type'] = 'checkbox';
+settingList['localize']['hide_seconds'] = {}
+settingList['localize']['hide_seconds']['title'] = 'Hide seconds';
+settingList['localize']['hide_seconds']['type'] = 'checkbox';
 
-settingList['clocks']['boss_stationclock'] = {}
-settingList['clocks']['boss_stationclock']['title'] = 'StationClock Type';
-settingList['clocks']['boss_stationclock']['type'] = 'text';
+settingList['localize']['hide_seconds_stationclock'] = {}
+settingList['localize']['hide_seconds_stationclock']['title'] = 'Hide seconds in StationClock';
+settingList['localize']['hide_seconds_stationclock']['type'] = 'checkbox';
 
-settingList['googlemaps'] = {}
-settingList['googlemaps']['title'] = 'Google Maps';
+settingList['localize']['boss_stationclock'] = {}
+settingList['localize']['boss_stationclock']['title'] = 'StationClock Type';
+settingList['localize']['boss_stationclock']['type'] = 'text';
 
-settingList['googlemaps']['gm_api'] = {}
-settingList['googlemaps']['gm_api']['title'] = 'API Key';
-settingList['googlemaps']['gm_api']['type'] = 'text';
+settingList['localize']['gm_api'] = {}
+settingList['localize']['gm_api']['title'] = 'API Key';
+settingList['localize']['gm_api']['type'] = 'text';
 
-settingList['googlemaps']['gm_zoomlevel'] = {}
-settingList['googlemaps']['gm_zoomlevel']['title'] = 'Zoom level';
-settingList['googlemaps']['gm_zoomlevel']['type'] = 'text';
+settingList['localize']['gm_zoomlevel'] = {}
+settingList['localize']['gm_zoomlevel']['title'] = 'Zoom level';
+settingList['localize']['gm_zoomlevel']['type'] = 'text';
 
-settingList['googlemaps']['gm_latitude'] = {}
-settingList['googlemaps']['gm_latitude']['title'] = 'Latitude';
-settingList['googlemaps']['gm_latitude']['type'] = 'text';
+settingList['localize']['gm_latitude'] = {}
+settingList['localize']['gm_latitude']['title'] = 'Latitude';
+settingList['localize']['gm_latitude']['type'] = 'text';
 
-settingList['googlemaps']['gm_longitude'] = {}
-settingList['googlemaps']['gm_longitude']['title'] = 'Longitude';
-settingList['googlemaps']['gm_longitude']['type'] = 'text';
-
-
-settingList['wunderground'] = {}
-settingList['wunderground']['title'] = 'Wunderground';
-
-settingList['wunderground']['wu_api'] = {}
-settingList['wunderground']['wu_api']['title'] = 'API Key';
-settingList['wunderground']['wu_api']['type'] = 'text';
-
-settingList['wunderground']['wu_city'] = {}
-settingList['wunderground']['wu_city']['title'] = 'Station (City or Code)';
-settingList['wunderground']['wu_city']['type'] = 'text';
-
-settingList['wunderground']['wu_name'] = {}
-settingList['wunderground']['wu_name']['title'] = 'Display Name';
-settingList['wunderground']['wu_name']['type'] = 'text';
-
-settingList['wunderground']['wu_country'] = {}
-settingList['wunderground']['wu_country']['title'] = 'Country';
-settingList['wunderground']['wu_country']['type'] = 'text';
+settingList['localize']['gm_longitude'] = {}
+settingList['localize']['gm_longitude']['title'] = 'Longitude';
+settingList['localize']['gm_longitude']['type'] = 'text';
 
 
-settingList['ziggo'] = {}
-settingList['ziggo']['title'] = 'Ziggo/UPC Mediabox XL (Horizon)';
+settingList['weather'] = {}
+settingList['weather']['title'] = 'Weather';
 
-settingList['ziggo']['switch_horizon'] = {}
-settingList['ziggo']['switch_horizon']['title'] = 'Path to switch_horizon.php';
-settingList['ziggo']['switch_horizon']['type'] = 'text';
-settingList['ziggo']['switch_horizon']['help'] = 'If you have a mediabox from ZIGGO or UPC (Horizon), copy switch_horizon.php on a webserver inside your network and change the IP (of Horizon) inside this file.';
+settingList['weather']['use_fahrenheit'] = {}
+settingList['weather']['use_fahrenheit']['title'] = 'Use Fahrenheit instead of Celcius';
+settingList['weather']['use_fahrenheit']['type'] = 'checkbox';
+
+settingList['weather']['use_beaufort'] = {}
+settingList['weather']['use_beaufort']['title'] = 'Use Bft instead of m/s';
+settingList['weather']['use_beaufort']['type'] = 'checkbox';
+
+settingList['weather']['wu_api'] = {}
+settingList['weather']['wu_api']['title'] = 'API Key';
+settingList['weather']['wu_api']['type'] = 'text';
+
+settingList['weather']['wu_city'] = {}
+settingList['weather']['wu_city']['title'] = 'Station (City or Code)';
+settingList['weather']['wu_city']['type'] = 'text';
+
+settingList['weather']['wu_name'] = {}
+settingList['weather']['wu_name']['title'] = 'Display Name';
+settingList['weather']['wu_name']['type'] = 'text';
+
+settingList['weather']['wu_country'] = {}
+settingList['weather']['wu_country']['title'] = 'Country';
+settingList['weather']['wu_country']['type'] = 'text';
 
 
-settingList['spotify'] = {}
-settingList['spotify']['title'] = 'Spotify';
+settingList['media'] = {}
+settingList['media']['title'] = 'Media';
 
-settingList['spotify']['spot_clientid'] = {}
-settingList['spotify']['spot_clientid']['title'] = 'ClientID';
-settingList['spotify']['spot_clientid']['type'] = 'text';
+settingList['media']['switch_horizon'] = {}
+settingList['media']['switch_horizon']['title'] = 'Ziggo/UPC Mediabox Path';
+settingList['media']['switch_horizon']['type'] = 'text';
+settingList['media']['switch_horizon']['help'] = 'If you have a mediabox from ZIGGO or UPC (Horizon), copy switch_horizon.php on a webserver inside your network and change the IP (of Horizon) inside this file.';
+
+settingList['media']['spot_clientid'] = {}
+settingList['media']['spot_clientid']['title'] = 'Spotify ClientID';
+settingList['media']['spot_clientid']['type'] = 'text';
+
+settingList['media']['hide_mediaplayer'] = {}
+settingList['media']['hide_mediaplayer']['title'] = 'Hide mediaplayer when it\'s off';
+settingList['media']['hide_mediaplayer']['type'] = 'checkbox';
 
 var settings = {};
 $.each(localStorage, function(key, value){
@@ -190,51 +186,78 @@ $(document).ready(function(){
 		$('.settingsicon').click('click');
 	}
 	var html = '<div class="modal fade" id="settings" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">';
-	  html+='<div class="modal-dialog">';
+	  html+='<div class="modal-dialog modal-dialog-settings">';
 		html+='<div class="modal-content">';
 		  html+='<div class="modal-header">';
 			html+='<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>';
 		  html+='</div>';
 		  html+='<div class="modal-body">';
 			html+='<div class="row">';
-		  	  for(b in settingList){
+			
+			
+			
+			  html+='<ul class="nav nav-pills">';
+				 var first=true;
+				 for(b in settingList){
+				 	var c='';
+					if(first){
+						c=' class="active"'; 
+						first=false;
+					}
+					html+='<li'+c+'><a data-toggle="pill" href="#tabs-'+b+'">'+settingList[b]['title']+'</a></li>';
+				 }
+			  html+='</ul>';
 			  
-				  html+='<div class="col-md-6 col-xs-12"><h4 style="margin:0px 0px 7px -1px;">'+settingList[b]['title']+'</h4>';
-				  
-				  for(s in settingList[b]){
-					  if(s!=='title'){
-							html+='<div class="row"><div class="col-xs-5" style="margin-bottom:1px;"><label style="margin-top: 6px;font-size: 12px;">'+settingList[b][s]['title']+'</label></div>';
-							html+='<div class="col-xs-7" style="margin-bottom:1px;">';
-							var val='';
-							if(typeof(settings[s])!=='undefined') val=settings[s];
-							if(settingList[b][s]['type']=='text') html+='<div><input class="form-control" type="text" name="'+s+'" value="'+val+'" style="max-width:95%;" /></div>';
-							if(settingList[b][s]['type']=='checkbox'){
-								var sel='';
-								if(settings[s]==1) sel='checked';
-								html+='<div class="material-switch pull-left"><input type="checkbox" id="'+s+'" name="'+s+'" value="1" '+sel+' style="margin-top:11px;" /><label for="'+s+'" class="label-success"></label></div>';
-							}
-							if(settingList[b][s]['type']=='select'){
-								html+='<select name="'+s+'" class="form-control" style="max-width:95%;padding-left: 8px;color: #555 !important;">';
-									html+='<option value=""></option>';
-									for(o in settingList[b][s]['options']){
-										var sel='';
-										if(settings[s]==o) sel='selected';
-										html+='<option value="'+o+'" '+sel+'>'+settingList[b][s]['options'][o]+'</option>';
-									}
-								html+='</select>';
-							}
-						  html+='</div></div>';
-					  }
-				  }
-				  
-				  html+='<br /><br /></div>';
+			  html+='<div class="tab-content"><br /><br />';
+			  
+			  var first=true;
+			  for(b in settingList){
+			  		var c='';
+					if(first){
+						c=' active in'; 
+						first=false;
+					}
+				  html+='<div class="tab-pane fade'+c+'" id="tabs-'+b+'">';
+					for(s in settingList[b]){
+						if(s!=='title'){
+							html+='<div class="row">';
+								html+='<div class="col-xs-5" style="margin-bottom:1px;"><label style="margin-top: 6px;font-size: 12px;">'+settingList[b][s]['title']+'</label>';
+								if(typeof(settingList[b][s]['help'])!=='undefined') html+='<span class="glyphicon" title="'+settingList[b][s]['help']+'">&nbsp;&#xe086;</span>';
+								html+='</div>';
+								html+='<div class="col-xs-7" style="margin-bottom:1px;">';
+								var val='';
+								if(typeof(settings[s])!=='undefined') val=settings[s];
+								if(settingList[b][s]['type']=='text') html+='<div><input class="form-control" type="text" name="'+s+'" value="'+val+'" style="max-width:75%;" /></div>';
+								if(settingList[b][s]['type']=='checkbox'){
+									var sel='';
+									if(settings[s]==1) sel='checked';
+									html+='<div class="material-switch pull-left"><input type="checkbox" id="'+s+'" name="'+s+'" value="1" '+sel+' style="margin-top:11px;" /><label for="'+s+'" class="label-success"></label></div>';
+								}
+								if(settingList[b][s]['type']=='select'){
+									html+='<select name="'+s+'" class="form-control" style="max-width:75%;padding-left: 8px;color: #555 !important;">';
+										html+='<option value=""></option>';
+										for(o in settingList[b][s]['options']){
+											var sel='';
+											if(settings[s]==o) sel='selected';
+											html+='<option value="'+o+'" '+sel+'>'+settingList[b][s]['options'][o]+'</option>';
+										}
+									html+='</select>';
+								}
+								html+='</div>';
+							html+='</div>';
+					  	}
+					}
+					//html+='<p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>';
+				  html+='</div>';
 			  }
 			html+='</div>';
-		  html+='<div class="text-right"><button onClick="saveSettings();" style="font-size: 16px;margin-right: 20px;margin-top: 30px;">Opslaan</button></div></div>';
+		  html+='</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> <button onClick="saveSettings();" type="button" class="btn btn-primary" data-dismiss="modal">Save changes</button></div>';
 		html+='</div>';
 	  html+='</div>';
 	html+='</div>';
 	$('body').append(html);
+	
+	$( "#tabs" ).tabs();
 
 });
 
