b8c4b631f6f45fc8232efceba80f5d2c9ea627e4
Rob Geerts <rob@wedevise.nl>
2017-08-06 22:16:09 +0200

    RGBWW support

--- js/main.js
+++ js/main.js
@@ -1212,7 +1212,7 @@ function getDevices(override){
 											html+=' / <span class="lastupdate">'+moment(device['LastUpdate']).format(settings['timeformat'])+'</span>';
 										}
 										html+='<br />';
-										if(device['SubType']=='RGBW'){
+										if(device['SubType']=='RGBW' || device['SubType']=='RGBWW'){
 											html+='<input type="text" class="rgbw" data-light="'+device['idx']+'" />';
 											html+='<div class="slider slider'+device['idx']+'" style="margin-left:55px;" data-light="'+device['idx']+'"></div>';
 										}
@@ -1225,7 +1225,7 @@ function getDevices(override){
 									$('div.block_'+idx).html(html);
 									addHTML=false;
 									
-									if(device['SubType']=='RGBW'){
+									if(device['SubType']=='RGBW' || device['SubType']=='RGBWW'){
 										$(".rgbw").spectrum({
 											color: Cookies.get('rgbw_'+idx)
 										});
