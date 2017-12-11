6fdc1b2f88312e017b6bb953f648f2aee7126f93
Rob Geerts <rob@wedevise.nl>
2017-12-11 12:13:29 +0100

    Get data from garbage companies via dashticz.nl so code will be shorter and bugs (regarding data) can be fixed remote without having to update.

--- js/garbage.js
+++ js/garbage.js
@@ -84,37 +84,10 @@ function getIcalData(address, date, random, url) {
     });
 }
 
-function getDeAfvalAppData(address, date, random) {
-    $.get(getPrefixUrl() + 'http://dataservice.deafvalapp.nl/dataservice/DataServiceServlet?type=ANDROID&service=OPHAALSCHEMA&land=NL&postcode=' + address.postcode + '&straatId=0&huisnr=' + address.housenumber + '&huisnrtoev=' + address.housenumberSuffix, function (data) {
-        var dataFiltered = [];
-
-        data.toString().split('\n').forEach(function (element) {
-            element = element.split(';');
-            var garbageType = element[0];
-            element
-                .filter(function (element) { return element.length})
-                .slice(1)
-                .forEach(function (dateElement) {
-                    dataFiltered.push({
-                        date: moment(dateElement, 'DD-MM-YYYY'),
-                        summary: garbageType.slice(0, 1).toUpperCase() + garbageType.slice(1).toLowerCase(),
-                        garbageType: mapGarbageType(garbageType),
-                    });
-            });
-        });
-        dataFiltered = dataFiltered
-            .filter(function (element) {
-                return element.date.isBetween(date.start, date.end, null, '[]');
-            });
-
-        addToContainer(random, dataFiltered);
-    });
-}
-
 function getTwenteMilieuData(address, date, random) {
     $.post('https://wasteapi.2go-mobile.com/api/FetchAdress', {
         'companyCode': '8d97bb56-5afd-4cbc-a651-b4f7314264b4',
-        'postCode': address.postcode,
+        'postCode': address.zipcode,
         'houseNumber': address.housenumber,
         'houseLetter': '',
         'houseNumberAddition': address.housenumberSuffix
@@ -147,30 +120,12 @@ function getTwenteMilieuData(address, date, random) {
     });
 }
 
-function getAfvalstromenData(address, date, random, baseUrl) {
-    $('.trash' + random + ' .state').html('');
-    $.getJSON(getPrefixUrl() + baseUrl + '/rest/adressen/' + address.postcode + '-' + address.housenumber, function (data) {
-        $.getJSON(getPrefixUrl() + baseUrl + '/rest/adressen/' + data[0].bagId + '/afvalstromen', function (data) {
-            data = data
-                .filter(function (element) { return element.ophaaldatum !== null; })
-                .map(function (element) {
-                    return {
-                        date: moment(element.ophaaldatum, 'YYYY-MM-DD'),
-                        summary: element.title,
-                        garbageType: mapGarbageType(element.title),
-                    };
-                });
-            addToContainer(random, data);
-        });
-    });
-}
-
 function getOphaalkalenderData(address, date, random) {
     $('.trash' + random + ' .state').html('');
     var baseURL = 'http://www.ophaalkalender.be';
 
-    $.getJSON(getPrefixUrl() + baseURL + '/calendar/findstreets/?query=' + address.street + '&zipcode=' + address.postcode, function (data) {
-        $.getJSON(getPrefixUrl() + baseURL + '/api/rides?id=' + data[0].Id + '&housenumber=0&zipcode=' + address.postcode, function (data) {
+    $.getJSON(getPrefixUrl() + baseURL + '/calendar/findstreets/?query=' + address.street + '&zipcode=' + address.zipcode, function (data) {
+        $.getJSON(getPrefixUrl() + baseURL + '/api/rides?id=' + data[0].Id + '&housenumber=0&zipcode=' + address.zipcode, function (data) {
             data = data.filter(function (element) {
                     return moment(element.start, 'YYYY-MM-DDTHH:mm:ss+-HH:mm').isBetween(date.start, date.end, null, '[]');
                 })
@@ -189,7 +144,7 @@ function getOphaalkalenderData(address, date, random) {
 function getAfvalwijzerArnhemData(address, date, random) {
     $('.trash' + random + ' .state').html('');
     var baseURL = 'http://www.afvalwijzer-arnhem.nl';
-    $.get(getPrefixUrl() + baseURL + '/applicatie?ZipCode=' + address.postcode + '&HouseNumber=' + address.housenumber + '&HouseNumberAddition=' + address.housenumberSuffix, function (data) {
+    $.get(getPrefixUrl() + baseURL + '/applicatie?ZipCode=' + address.zipcode + '&HouseNumber=' + address.housenumber + '&HouseNumberAddition=' + address.housenumberSuffix, function (data) {
         var returnDates = [];
         $(data).find('ul.ulPickupDates li').each(function (index, element) {
             returnDates.push({
@@ -202,96 +157,28 @@ function getAfvalwijzerArnhemData(address, date, random) {
     });
 }
 
-function getMijnAfvalwijzerData(address, date, random) {
-    $.getJSON(getPrefixUrl() + 'http://json.mijnafvalwijzer.nl/?method=postcodecheck&postcode=' + address.postcode + '&street=&huisnummer=' + address.housenumber + '&toevoeging=' + address.housenumberSuffix, function (data) {
-        data = data.data.ophaaldagen.data
+function getGeneralData(service,address, date, random, subservice){
+	$.getJSON(getPrefixUrl() + 'http://dashticz.nl/afval/?service='+service+'&sub='+subservice+'&zipcode=' + address.zipcode + '&nr=' + address.housenumber + '&t=' + address.housenumberSuffix, function (data) {
+		 data = data
             .filter(function (element) {
                 return moment(element.date).isBetween(date.start, date.end, null, '[]');
             })
             .map(function (element) {
                 return {
                     date: moment(element.date),
-                    summary: element.nameType,
-                    garbageType: mapGarbageType(element.type),
+                    summary: element.title,
+                    garbageType: mapGarbageType(element.title),
                 };
             });
         addToContainer(random, data);
-    });
-}
-
-function getHvcData(address, date, random) {
-    $.getJSON(getPrefixUrl() + 'http://inzamelkalender.hvcgroep.nl/push/calendar?postcode=' + address.postcode + '&huisnummer=' + address.housenumber, function (data) {
-        var dataFiltered = [];
-        data.forEach(function (element) {
-            var seen = {};
-            element.dateTime.forEach(function (dateElement) {
-                this.date = moment(dateElement.date, 'YYYY-MM-DD HH:mm:ss');
-
-                if (!seen.hasOwnProperty(this.date.format('YYYY-MM_DD'))) {
-                    dataFiltered.push({
-                        date: this.date,
-                        summary: element.naam,
-                        garbageType: mapGarbageType(element.code),
-                    });
-                    seen[this.date.format('YYYY-MM_DD')] = true;
-                }
-            });
-        });
-        addToContainer(random, dataFiltered);
-    });
-}
-
-function getRovaData(address, date, random) {
-    $.getJSON('https://wedevise.nl/dashticz/rova.php?zipcode=' + address.postcode + '&number=' + address.housenumber, function (data) {
-        data = data.map(function (element) {
-            return {
-                date: moment(element.Date, 'YYYY-MM-DDTHH:mm:ss'),
-                summary: element.GarbageType,
-                garbageType: mapGarbageType(element.GarbageType),
-            };
-        });
-        addToContainer(random, data);
-    });
-}
-
-/**
- * Will be discontinued from 2018
- */
-function getRecycleManagerData(address, date, random) {
-    $.getJSON('https://vpn-wec-api.recyclemanager.nl/v2/calendars?postalcode=' + address.postcode + '&number=' + address.housenumber, function (data) {
-        var dataFiltered = [];
-        data.data.forEach(function (element) {
-            element.occurrences.forEach(function (occurrence) {
-                dataFiltered.push({
-                    date: moment(occurrence.from.date, 'YYYY-MM-DDTHH:mm:ss.SSS'),
-                    summary: occurrence.title,
-                    garbageType: mapGarbageType(occurrence.title),
-                });
-            });
-        });
-        addToContainer(random, dataFiltered);
-    });
-}
-
-function getEdgData(address, date, random) {
-    $.getJSON(getPrefixUrl() + 'https://www.edg.de/JsonHandler.ashx?dates=1&street=' + address.street + '&nr=' + address.housenumber + '&cmd=findtrash&tbio=0&tpapier=1&trest=1&twert=1&feiertag=0', function (data) {
-        data = data.data
-            .map(function (element) {
-                return {
-                    date: moment(element.date, 'DD.MM.YYYY'),
-                    summary: element.fraktion[0],
-                    garbageType: mapGarbageType(element.fraktion[0]),
-                }
-            });
-        addToContainer(random, data);
-    });
+	});
 }
 
 function getZuidhornData(address, date, random, fetchType) {
     var prefix = 'https://afvalkalender.zuidhorn.nl/';
 
     $.post(getPrefixUrl() + prefix + 'afvalkalender-zoeken/zoek-postcode/Zipcode/search.html', {
-        'tx_windwastecalendar_pi1[zipcode]': address.postcode,
+        'tx_windwastecalendar_pi1[zipcode]': address.zipcode,
         'tx_windwastecalendar_pi1[housenumber]': address.housenumber,
     }, function (data) {
         switch (fetchType) {
@@ -315,6 +202,40 @@ function getZuidhornData(address, date, random, fetchType) {
             break;
         }
     });
+}
+
+///http://dashticz.nl/afval/?service=afvalstromen&sub=alphenaandenrijn&zipcode=2401AR&nr=261&t=
+function getAfvalstromenData(address, date, random, service) {
+    getGeneralData('afvalstromen',address, date, random, service);
+}
+
+//http://dashticz.nl/afval/?service=deafvalapp&zipcode=5692VG&nr=33&t=
+function getDeAfvalAppData(address, date, random) {
+    getGeneralData('deafvalapp',address, date, random);
+}
+
+//http://dashticz.nl/afval/?service=mijnafvalwijzer&zipcode=3825AL&nr=41&t=
+function getMijnAfvalwijzerData(address, date, random) {
+    getGeneralData('mijnafvalwijzer',address, date, random);
+}
+
+//http://dashticz.nl/afval/?service=hvc&zipcode=1671jv&nr=22&t=
+function getHvcData(address, date, random) {
+    getGeneralData('hvc',address, date, random);
+}
+
+//http://dashticz.nl/afval/?service=rova&zipcode=7731ZT&nr=84&t=
+function getRovaData(address, date, random) {
+    getGeneralData('rova',address, date, random);
+}
+
+//http://dashticz.nl/afval/?service=recyclemanager&zipcode=3161lh&nr=27&t=
+function getRecycleManagerData(address, date, random) {
+    getGeneralData('recyclemanager',address, date, random);
+}
+
+function getEdgData(address, date, random) {
+    getGeneralData('edg',address, date, random);
 }
 
 function getTrashRow(garbage) {
@@ -392,24 +313,12 @@ function getMaxItems() {
     return 5;
 }
 
-function getOmriData(address, date, random) {
-    // $.post('http://www.omrin.nl/bij-mij-thuis/services/afvalkalender/',{
-    //     'zipcode': address.postcode.substr(0,4),
-    //     'zipcodeend':address.postcode.substr(4,6),
-    //     'housenumber':address.housenumber,
-    //     'addition':'',
-    //     'send':'Mijn overzicht'
-    // }, function(data) {
-    //     console.log(data);
-    // });
-}
-
 function loadDataForService(service, random) {
     var address = {
         street: settings['garbage_street'] || '',
         housenumber: settings['garbage_housenumber'] || '',
         housenumberSuffix: settings['garbage_housenumberadd'] || '',
-        postcode: settings['garbage_zipcode'] || '',
+        zipcode: settings['garbage_zipcode'] || '',
     };
     var date = {
         start: moment().startOf('day'),
@@ -422,37 +331,36 @@ function loadDataForService(service, random) {
         gemertbakelmaandag: {dataHandler: 'getIcalData', identifier: 'https://calendar.google.com/calendar/ical/o44qrtdhls8saftmesm5rqb85o%40group.calendar.google.com/public/basic.ics'},
         gemertbakeldinsdag: {dataHandler: 'getIcalData', identifier: 'https://calendar.google.com/calendar/ical/6p8549rssv114ddevingime95o%40group.calendar.google.com/public/basic.ics'},
         gemertbakelwoensdag: {dataHandler: 'getIcalData', identifier: 'https://calendar.google.com/calendar/ical/cv40f4vaie10v54f72go6ipb78%40group.calendar.google.com/public/basic.ics'},
-        veldhoven: {dataHandler: 'getIcalData', identifier: 'https://www.veldhoven.nl/afvalkalender/' + moment().format('YYYY') + '/' + address.postcode + '-' + address.housenumber + '.ics'},
-        best: {dataHandler: 'getIcalData', identifier: 'https://www.gemeentebest.nl/afvalkalender/' + moment().format('YYYY') + '/' + address.postcode + '-' + address.housenumber + '.ics'},
-        uden: {dataHandler: 'getIcalData', identifier: 'https://www.uden.nl/inwoners/afval/ophaaldagen-afval/' + moment().format('YYYY') + address.postcode + '-' + address.housenumber + '.ics'},
-        vianen: {dataHandler: 'getIcalData', identifier: 'https://www.vianen.nl/afval/afvalkalender/' + moment().format('YYYY') + address.postcode + '-' + address.housenumber + '.ics'},
-        goes: {dataHandler: 'getIcalData', identifier: 'http://afvalkalender.goes.nl/' + moment().format('YYYY') + address.postcode + '-' + address.housenumber + '.ics'},
-        deurne: {dataHandler: 'getIcalData', identifier: 'http://afvalkalender.deurne.nl/Afvalkalender/download_ical.php?p=' + address.postcode + '&h=' + address.housenumber + '&t=&jaar=' + moment().format('YYYY')},
-        heezeleende: {dataHandler: 'getIcalData', identifier: 'http://afvalkalender.heeze-leende.nl/Afvalkalender/download_ical.php?p=' + address.postcode + '&h=' + address.housenumber + '&t=&jaar=' + moment().format('YYYY')},
-        deafvalapp: {dataHandler: 'getDeAfvalAppData', identifier: ''},
+        veldhoven: {dataHandler: 'getIcalData', identifier: 'https://www.veldhoven.nl/afvalkalender/' + moment().format('YYYY') + '/' + address.zipcode + '-' + address.housenumber + '.ics'},
+        best: {dataHandler: 'getIcalData', identifier: 'https://www.gemeentebest.nl/afvalkalender/' + moment().format('YYYY') + '/' + address.zipcode + '-' + address.housenumber + '.ics'},
+        uden: {dataHandler: 'getIcalData', identifier: 'https://www.uden.nl/inwoners/afval/ophaaldagen-afval/' + moment().format('YYYY') + address.zipcode + '-' + address.housenumber + '.ics'},
+        vianen: {dataHandler: 'getIcalData', identifier: 'https://www.vianen.nl/afval/afvalkalender/' + moment().format('YYYY') + address.zipcode + '-' + address.housenumber + '.ics'},
+        goes: {dataHandler: 'getIcalData', identifier: 'http://afvalkalender.goes.nl/' + moment().format('YYYY') + address.zipcode + '-' + address.housenumber + '.ics'},
+        deurne: {dataHandler: 'getIcalData', identifier: 'http://afvalkalender.deurne.nl/Afvalkalender/download_ical.php?p=' + address.zipcode + '&h=' + address.housenumber + '&t=&jaar=' + moment().format('YYYY')},
+        heezeleende: {dataHandler: 'getIcalData', identifier: 'http://afvalkalender.heeze-leende.nl/Afvalkalender/download_ical.php?p=' + address.zipcode + '&h=' + address.housenumber + '&t=&jaar=' + moment().format('YYYY')},
         twentemilieu: {dataHandler: 'getTwenteMilieuData', identifier: ''},
-        cure: {dataHandler: 'getAfvalstromenData', identifier: 'https://afvalkalender.cure-afvalbeheer.nl'},
-        cyclusnv: {dataHandler: 'getAfvalstromenData', identifier: 'https://afvalkalender.cyclusnv.nl'},
-        gemeenteberkelland: {dataHandler: 'getAfvalstromenData', identifier: 'https://afvalkalender.gemeenteberkelland.nl'},
-        meerlanden: {dataHandler: 'getAfvalstromenData', identifier: 'https://afvalkalender.meerlanden.nl'},
-        venray: {dataHandler: 'getAfvalstromenData', identifier: 'https://afvalkalender.venray.nl'},
-        circulusberkel: {dataHandler: 'getAfvalstromenData', identifier: 'https://afvalkalender.circulus-berkel.nl'},
-        rmn: {dataHandler: 'getAfvalstromenData', identifier: 'https://inzamelschema.rmn.nl'},
-        alphenaandenrijn: {dataHandler: 'getAfvalstromenData', identifier: 'http://afvalkalender.alphenaandenrijn.nl'},
-        sudwestfryslan: {dataHandler: 'getAfvalstromenData', identifier: 'http://afvalkalender.sudwestfryslan.nl'},
-        dar: {dataHandler: 'getAfvalstromenData', identifier: 'https://afvalkalender.dar.nl'},
-        waalre: {dataHandler: 'getAfvalstromenData', identifier: 'https://afvalkalender.waalre.nl'},
-        avalex: {dataHandler: 'getAfvalstromenData', identifier: 'https://www.avalex.nl'},
         ophaalkalender: {dataHandler: 'getOphaalkalenderData', identifier: ''},
         afvalwijzerarnhem: {dataHandler: 'getAfvalwijzerArnhemData', identifier: ''},
-        mijnafvalwijzer: {dataHandler: 'getMijnAfvalwijzerData', identifier: ''},
-        hvc: {dataHandler: 'getHvcData', identifier: ''},
-        rova: {dataHandler: 'getRovaData', identifier: ''},
-        recyclemanager: {dataHandler: 'getRecycleManagerData', identifier: ''},
-        edg: {dataHandler: 'getEdgData', identifier: ''},
-        omri: {dataHandler: 'getOmriData', identifier: ''},
         zuidhornical: {dataHandler: 'getZuidhornData', identifier: 'ical'},
         zuidhorn: {dataHandler: 'getZuidhornData', identifier: 'scrape'},
+		deafvalapp: {dataHandler: 'getDeAfvalAppData', identifier: ''},
+        cure: {dataHandler: 'getAfvalstromenData', identifier: 'cure'},
+        cyclusnv: {dataHandler: 'getAfvalstromenData', identifier: 'cyclusnv'},
+        gemeenteberkelland: {dataHandler: 'getAfvalstromenData', identifier: 'gemeenteberkelland'},
+        meerlanden: {dataHandler: 'getAfvalstromenData', identifier: 'meerlanden'},
+        venray: {dataHandler: 'getAfvalstromenData', identifier: 'venray'},
+        circulusberkel: {dataHandler: 'getAfvalstromenData', identifier: 'circulusberkel'},
+        rmn: {dataHandler: 'getAfvalstromenData', identifier: 'rmn'},
+        alphenaandenrijn: {dataHandler: 'getAfvalstromenData', identifier: 'alphenaandenrijn'},
+        sudwestfryslan: {dataHandler: 'getAfvalstromenData', identifier: 'sudwestfryslan'},
+        dar: {dataHandler: 'getAfvalstromenData', identifier: 'dar'},
+        waalre: {dataHandler: 'getAfvalstromenData', identifier: 'waalre'},
+        avalex: {dataHandler: 'getAfvalstromenData', identifier: 'avalex'},
+        hvc: {dataHandler: 'getHvcData', identifier: ''},
+        rova: {dataHandler: 'getRovaData', identifier: ''},
+        mijnafvalwijzer: {dataHandler: 'getMijnAfvalwijzerData', identifier: ''},
+        recyclemanager: {dataHandler: 'getRecycleManagerData', identifier: ''},
+        edg: {dataHandler: 'getEdgData', identifier: ''},
     };
     window[serviceProperties[service].dataHandler](address, date, random, serviceProperties[service].identifier);
 }
