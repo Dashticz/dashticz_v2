6738e0f23944163dabb614cb4e7acee37ed06982
Henri de Jong <henridejong@gmail.com>
2017-12-12 20:03:09 +0000

    Fix some issues with the thermostat

--- js/thermostat.js
+++ js/thermostat.js
@@ -1,57 +1,49 @@
-function addThermostatFunctions(thermelement){					
-	$(document).delegate((thermelement+' .btn-number'),"click",function(e){
-		sliding=true;
-	  fieldName = $(this).attr('data-field');
-	  type = $(this).attr('data-type');
-	  var input = $(thermelement+" strong");
-	  var currentVal = input.text().split('°');
-	  currentVal = parseFloat(currentVal[0]);
-	  if (!isNaN(currentVal)) {
-		if (type == 'minus') {
+function addThermostatFunctions(thermelement) {
+    $(document).delegate((thermelement + ' .btn-number'), "click", function (e) {
+        sliding = true;
+        var fieldName = $(this).attr('data-field');
+        var type = $(this).attr('data-type');
+        var input = $(thermelement + " strong");
+        var currentVal = input.text().split('°');
+        currentVal = parseFloat(currentVal[0].replace(',', '.'));
+        if (!isNaN(currentVal)) {
+            var newValue = (type === 'minus') ? currentVal - 0.5 : currentVal + 0.5;
+            if (newValue >= input.attr('min')
+                && newValue <= input.attr('max')
+            ) {
+                input.text(number_format(newValue, 1) + _TEMP_SYMBOL).change();
+                switchThermostat(newValue, input);
+            }
+            if (newValue <= input.attr('min')) {
+                $(this).attr('disabled', true);
+            }
+            if (newValue >= input.attr('max')) {
+                $(this).attr('disabled', true);
+            }
+        } else {
+            input.text(0);
+        }
+    });
 
-		  if (currentVal > input.attr('min')) {
-			input.text(currentVal - 0.5 + _TEMP_SYMBOL).change();
-			switchThermostat(parseFloat(input.text()),input);
-		  }
-		  if (parseFloat(input.text()) == input.attr('min')) {
-			$(this).attr('disabled', true);
-		  }
+    $(thermelement + ' .input-number').focusin(function () {
+        $(this).data('oldValue', $(this).text());
+    });
 
-		} else if (type == 'plus') {
+    $(thermelement + ' .input-number').change(function () {
+        minValue = parseFloat($(this).attr('min'));
+        maxValue = parseFloat($(this).attr('max'));
+        valueCurrent = parseFloat($(this).text());
 
-		  if (currentVal < input.attr('max')) {
-			input.text(currentVal + 0.5 + _TEMP_SYMBOL).change();
-			switchThermostat(parseFloat(input.text()),input);
-		  }
-		  if (parseFloat(input.text()) == input.attr('max')) {
-			$(this).attr('disabled', true);
-		  }
-
-		}
-	  } else {
-		input.text(0);
-	  }
-	});
-
-	$(thermelement+' .input-number').focusin(function() {
-	  $(this).data('oldValue', $(this).text());
-	});
-
-	$(thermelement+' .input-number').change(function() {
-	  minValue = parseFloat($(this).attr('min'));
-	  maxValue = parseFloat($(this).attr('max'));
-	  valueCurrent = parseFloat($(this).text());
-
-	  name = $(this).attr('name');
-	  if (valueCurrent >= minValue) {
-		$(thermelement+" .btn-number[data-type='minus']").removeAttr('disabled')
-	  } else {
-		  $(this).val($(this).data('oldValue'));
-	  }
-	  if (valueCurrent <= maxValue) {
-		$(thermelement+" .btn-number[data-type='plus']").removeAttr('disabled')
-	  } else {
-		  $(this).val($(this).data('oldValue'));
-	  }
-	});
+        name = $(this).attr('name');
+        if (valueCurrent >= minValue) {
+            $(thermelement + " .btn-number[data-type='minus']").removeAttr('disabled')
+        } else {
+            $(this).val($(this).data('oldValue'));
+        }
+        if (valueCurrent <= maxValue) {
+            $(thermelement + " .btn-number[data-type='plus']").removeAttr('disabled')
+        } else {
+            $(this).val($(this).data('oldValue'));
+        }
+    });
 }
