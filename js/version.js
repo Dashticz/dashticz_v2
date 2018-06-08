430c69116ebe3d99b1c2a36930b615cafc3bbcf5
DewGew <pierlar@hotmail.com>
2018-06-08 10:24:22 +0200

    Rewrite version checker
    Easier for developer to update version. Just change the json data in version.txt

--- js/version.js
+++ js/version.js
@@ -1,12 +1,11 @@
 
 	/*
-	Check the SECOND latest commit on github because the latest commit is updating ref_commit in this file.
-	Next this file is reading comments from the latest commit (update ref_commit) eg. "New function - Update checker"
+	Check the latest version of dashticz on github.
+	Check domoticz version
 	*/
 	
-var dashticz_version = 'V2.3.5';
-var dashticz_branch = 'beta'; /* master or beta */
-var ref_commit = '2dc5f130afad00d852b8427cc8f3bbc7f6eefacb' /* Reference commit - add the latest commit BEFORE make a PR of this file */
+var dashticz_version;
+var dashticz_branch;
 var newVersion = '';
 var moved = false;
 var loginCredentials ='';
@@ -15,17 +14,23 @@ var dzVents = '';
 var python = '';
 var levelNamesEncoded = false;
 var levelNamesEncodeVersion = '3.9476' /* Domoticz version above this, level names are encoded */
+
+$.ajax({url: 'js/version.txt' , async: false, dataType: 'json', success: function(localdata) {
+
+	dashticz_version = localdata.version;
+	dashticz_branch = localdata.branch
+	}
+});
 	
-$.ajax({url: 'https://api.github.com/repos/Dashticz/dashticz_v2/branches/' + dashticz_branch, async: false, dataType: 'json', success: function(data) {
+$.ajax({url: 'https://raw.githubusercontent.com/Dashticz/dashticz_v2/'+ dashticz_branch +'/version.txt' , async: false, dataType: 'json', success: function(data) {
 	
-	dashticz_branch = data['name']
-	var message = 'Last change made: ' + data['commit']['commit']['message'];
+	var message = 'Last change made: ' + data.last_changes;
 	
-	if (ref_commit !== data['commit']['parents'][0]['sha']) {
+	if (dashticz_version !== data.version) {
 		moved = true;
 		newVersion = '<br><i>New version is available! <a href="https://github.com/Dashticz/dashticz_v2/tree/' + dashticz_branch + '" target="_blank">Click here to download</a></i><br><i>' + message + '</i>';
 	}
-	else if (ref_commit === data['commit']['parents'][0]['sha']) {
+	else if (dashticz_version === data.version) {
 		moved = false;
 		newVersion = '<br><i>You are running latest version.</i>';
 	}
@@ -44,5 +49,7 @@ $.ajax({url: config['domoticz_ip'] + '/json.htm?' + loginCredentials + 'type=com
 	dzVents = '<br>dzVents version: ' + data.dzvents_version;
 	python = '<br> Python version: ' + data.python_version;
 	if (domoversion >= levelNamesEncodeVersion) levelNamesEncoded = true; else levelNamesEncoded = false;
-}
+	
+	}
 });
+© 2018 GitHub, Inc.
