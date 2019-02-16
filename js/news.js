7c706fb8fcc5f7d00f65e3d8f140f1da8dc333fd
Sander Lokerse <slokerse@gmail.com>
2019-02-16 13:24:11 +0100

    Fix news height for refresh while in standby

--- js/news.js
+++ js/news.js
@@ -67,7 +67,9 @@ function getNews(divToFill, newsfeed) {
                     maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
                 });
 
-                $('#rss-styled_' + divToFill).parents('.transbg').height(maxHeight);
+                if(maxHeight > 0) {
+                  $('#rss-styled_' + divToFill).parents('.transbg').height(maxHeight);
+                }
             },error: function(data){
 		infoMessage('<font color="red">News Error!</font>','RSS feed ' + data.statusText +'. Check rss url.', 10000);
 		}
